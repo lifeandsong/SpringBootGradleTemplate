67febd9ff88067c8078d7e710e161171a7d92b5f
lifeandsong <lifeandsong@gmail.com>
2022-07-01 04:50:06 +0900

    swagger 설정 오류 수정

--- src/main/java/org/swmaestro/demo/config/SwaggerConfig.java
+++ src/main/java/org/swmaestro/demo/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package org.swmaestro.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
